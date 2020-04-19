06a7836a7663931468503c0bc84fd3673305b6c3
si664 <si664project@8e86bfb0e784>
2020-04-19 01:04:15 +0000

    changes

--- fightcovid/qas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from qas.models import Question, Answer, Tag
-
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(Tag)
--- fightcovid/qas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QasConfig(AppConfig):
-    name = 'qas'
